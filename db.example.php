75f89413878e87d1a7b3df2563977de9d771a2a7
Stefan Stoyanov <stefanstoyanovpro@gmail.com>
2026-09-25 09:44:08 -0400

    Move DB credentials to environment variables (Apache SetEnv)

--- db.example.php
+++ db.example.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "devticket_user";
-$pass = "Password123";
-$db   = "devticket";
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$db   = getenv("DB_NAME");
 
 $conn = new mysqli($host, $user, $pass, $db);
 
